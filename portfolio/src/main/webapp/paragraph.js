660547aa9b6eb0db571f4503a4fd04d04d73c818
Wu Sibing <1915563868@qq.com>
2020-06-02 10:26:42 +0000

    Refactor code.

--- portfolio/src/main/webapp/paragraph.js
+++ portfolio/src/main/webapp/paragraph.js
@@ -2,45 +2,31 @@
  * Adds a heading with a paragraph to each picture.
  */
 function addDescription(directory) {
-    let imagesAndMapping = getImagesnMapping(directory);
-    let files = imagesAndMapping[0];
-    let mapping = imagesAndMapping[1];
-    let size = files.length;
+    let imagePathDescriptionMap = getImageDescriptionMap(directory);
 
     generateDiv("content");
 
-    for (let i = 0; i < size; i++) {
-        generateHTMLLayout(files[i], mapping, i, "description");
-    }
+    let count = 1;
+    for (const [imagePath, description] of Object.entries(imagePathDescriptionMap)) {
+        generateHTMLLayout(imagePath, description, count, "description");
+        count++;
+    };
 }
 
 /**
  * Gets the images to be displayed.
  * @param {string} directory  directory to the images.
- * @return {array of string array} an array containing image paths and image-description mapping
+ * @return {array of string array} a map from image path to image description
  */
-function getImagesnMapping(directory) {
-    let fileNames = ["sibing-1.jpg",
-                     "swag.jpg",
-                     "welcomePackage.jpg",
-                     "sibing-2.jpg"];
-
-    let descriptions = ["This picture was taken accidentally by my friend two years ago. However it becomes one of my favorite photo since at that time I was still quite slim.",
-                        "Me & Welcome Packge with Google!",
-                        "Welcome package from Google!",
-                        "My favorite ID photo, since I was truly slim at that time."];
-    
-    let files = [];
-    let mapping = {};
-    for (let i = 0; i < fileNames.length; i++) {
-        let filePath = directory + "/" + fileNames[i];
-        files.push(filePath);
-        mapping[filePath] = descriptions[i];
+function getImageDescriptionMap(directory) {
+    let mapping = {
+        "images/sibing-1.jpg": "This picture was taken accidentally by my friend two years ago. However it becomes one of my favorite photo since at that time I was still quite slim.",
+        "images/swag.jpg": "Me & Welcome Packge with Google!",
+        "images/welcomePackage.jpg": "Welcome package from Google!",
+        "images/sibing-2.jpg": "My favorite ID photo, since I was truly slim at that time."
     }
 
-    let result = [files, mapping];
-
-    return result;
+    return mapping;
 }
 
 /**
@@ -60,22 +46,22 @@ function generateDiv(elementId) {
 /**
  * Generates the layout in the html.
  * @param {string} image path to the image.
- * @param {dictionary} mapping images and description mapping.
- * @param {number} i index in the file string array.
+ * @param {string} description description to the image.
+ * @param {number} i paragraph id.
  * @param {string} elementId id of the element where the paragraphs place.
  */
-function generateHTMLLayout(image, mapping, i, elementId) {
+function generateHTMLLayout(image, description, i, elementId) {
     let tag = document.createElement("a");
     let source = document.createAttribute("href");
     source.value = image; 
     tag.setAttributeNode(source);
-    tag.innerHTML = "<h3>P" + (i + 1) + "</h3>";
+    tag.innerHTML = "<h3>P" + i + "</h3>";
 
-    let description = document.createElement("p");
-    let text = document.createTextNode(mapping[image]);
-    description.appendChild(text);
+    let descriptionText = document.createElement("p");
+    let text = document.createTextNode(description);
+    descriptionText.appendChild(text);
 
     let element = document.getElementById(elementId);
     element.appendChild(tag);
-    element.appendChild(description);
+    element.appendChild(descriptionText);
 }
