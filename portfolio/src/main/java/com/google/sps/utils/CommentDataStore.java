7fdffe79816b1dabed93f13bca951a47e99506f8
Wu Sibing <1915563868@qq.com>
2020-06-09 12:27:28 +0800

    Refactor code

--- portfolio/src/main/java/com/google/sps/utils/CommentDataStore.java
+++ portfolio/src/main/java/com/google/sps/utils/CommentDataStore.java
@@ -20,7 +20,7 @@ public class CommentDataStore implements ObjectDataStore<Comment> {
     }
 
     /**
-     * Puts the entity into Datastore.
+     * Puts the comments into Datastore.
      * @param comment Comment object needed to be stored.
      */
     @Override
@@ -29,7 +29,7 @@ public class CommentDataStore implements ObjectDataStore<Comment> {
     }
 
     /**
-     * Loads the entity from Datastore.
+     * Loads the comments from Datastore.
      * @return An Iterable.
      */
     @Override
--- portfolio/src/main/java/com/google/sps/utils/ObjectDataStore.java
+++ portfolio/src/main/java/com/google/sps/utils/ObjectDataStore.java
@@ -2,16 +2,18 @@ package com.google.sps.utils;
 
 import java.util.List;
 
-/** Interface that provides the service to interact with Datastore to store or load object. */
+/** Interface for classes whose instances can be written to and restored from Datastore.
+ * Classes implementing the ObjectDataStore interface must also have a non-null static field called CREATOR of a type
+ * that implements the EntityConvertibleCreator interface. */
 public interface ObjectDataStore<T> {
     /**
-     * Puts the entity into Datastore.
+     * Puts the object into Datastore.
      * @param item Object needed to be stored.
      */
     public void store(T item);
 
     /**
-     * Loads the entity from Datastore.
+     * Loads the object from Datastore.
      * @return An list of objects.
      */
     public List<T> load();
