0ddf699b97222d4c3341457c682f1e5f69e4d94d
Wu Sibing <1915563868@qq.com>
2020-06-09 23:37:34 +0800

    Correct accidental typo

--- portfolio/src/main/java/com/google/sps/servlets/DeleteCommentServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteCommentServlet.java
@@ -3,12 +3,13 @@ package com.google.sps.servlets;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /** Servlet that handles deleting comment content. */
 @WebServlet("/delete-comment")
-public class DeleteCommentServlet extends CommentServlet {
+public class DeleteCommentServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
