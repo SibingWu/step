46ad524b5ddfcada05a28e761992ee4deee724ed
Wu Sibing <1915563868@qq.com>
2020-06-11 21:13:12 +0800

    Modify redirect link

--- portfolio/src/main/java/com/google/sps/authentication/servlets/LoginServlet.java
+++ portfolio/src/main/java/com/google/sps/authentication/servlets/LoginServlet.java
@@ -45,7 +45,7 @@ public final class LoginServlet extends HttpServlet {
             htmlText = String.format("<p>Hello %s!</p>\n"
                     + "<p>Logout <a href=\"%s\">here</a>.</p>", userEmail, logoutUrl);
         } else {
-            String urlToRedirectToAfterUserLogsIn = "/comment.html";
+            String urlToRedirectToAfterUserLogsIn = "/";
             String loginUrl = userService.createLoginURL(urlToRedirectToAfterUserLogsIn);
 
             htmlText = String.format("<p>Hello stranger.</p>"
