8b5cf3756c978f09826570c2d239fc03e80548c4
Wu Sibing <1915563868@qq.com>
2020-06-12 17:42:28 +0800

    Refactor the code

--- portfolio/src/main/webapp/map.js
+++ portfolio/src/main/webapp/map.js
@@ -1,10 +1,10 @@
 /** Creates a map and adds it to the page. */
 function createMap() {
-  const map = new google.maps.Map(
-      document.getElementById('map'),
-      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+    const map = new google.maps.Map(
+        document.getElementById('map'),
+        {center: {lat: 37.422, lng: -122.084}, zoom: 16});
 
-  const trexMarker = new google.maps.Marker({
+    const trexMarker = new google.maps.Marker({
       position: {lat: 37.421903, lng: -122.084674},
       map: map,
       draggable: true,
@@ -12,7 +12,9 @@ function createMap() {
       title: "Stan the T-Rex"
     });
 
-  trexMarker.addListener("click", toggleBounce);
+    trexMarker.addListener("click", function() {
+        toggleBounce(trexMarker);
+    });
 }
 
 /**
