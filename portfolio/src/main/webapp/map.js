fcc7a6c6b66207d7dcfa4153d0eaf1d304b98e7b
Wu Sibing <1915563868@qq.com>
2020-06-12 17:24:14 +0800

    Add a draggable marker with drop & bouncing effect

--- portfolio/src/main/webapp/map.js
+++ portfolio/src/main/webapp/map.js
@@ -3,4 +3,24 @@ function createMap() {
   const map = new google.maps.Map(
       document.getElementById('map'),
       {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+
+  const trexMarker = new google.maps.Marker({
+      position: {lat: 37.421903, lng: -122.084674},
+      map: map,
+      draggable: true,
+      animation: google.maps.Animation.DROP,
+      title: 'Stan the T-Rex'
+    });
+}
+
+/**
+ * Generates the bouncing animation for marker.
+ * @param {marker} marker Target marker.
+ */
+function toggleBounce(marker) {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
 }
