80a5e2f1f1c42e88bd74c7841fc394435d3398d7
Wu Sibing <1915563868@qq.com>
2020-06-12 10:56:14 +0800

    Make DeleteCommentServlet final

--- portfolio/src/main/java/com/google/sps/servlets/DeleteCommentServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteCommentServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 
 /** Servlet that handles deleting comment content. */
 @WebServlet("/delete-comment")
-public class DeleteCommentServlet extends HttpServlet {
+public final class DeleteCommentServlet extends HttpServlet {
     private static final String KIND = "Comment";
     // TODO: actually this constant is used in both Comment and DeleteCommentServlet,
     //  seems not proper to put it private static final, how i shall do it?
