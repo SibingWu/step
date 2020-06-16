504aa9cf15b38de2ca0ecd916f4a2f4a4f35124f
Wu Sibing <1915563868@qq.com>
2020-06-16 14:51:10 +0800

    Implement FindMeetingQuery

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,92 @@
 
 package com.google.sps;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    // throw new UnsupportedOperationException("TODO: Implement this method.");
+
+    Collection<TimeRange> availableMeetings = new ArrayList<>();
+
+    Collection<String> attendees = request.getAttendees();
+    long duration = request.getDuration();
+
+    List<TimeRange> occupiedTimeRange = new ArrayList<>();
+
+    for (Event event: events) {
+      // Ignores those events which do not have overlapping attendees
+      if (!hasAttendeesAttending(event.getAttendees(), attendees)) {
+        continue;
+      }
+
+      occupiedTimeRange.add(event.getWhen());
+    }
+
+    List<TimeRange> mergedTimeRanges = mergeTimeRange(occupiedTimeRange);
+
+    int start = TimeRange.START_OF_DAY;
+    int end = TimeRange.START_OF_DAY;
+    for (TimeRange timeRange: mergedTimeRanges) {
+      TimeRange availableMeeting = TimeRange.fromStartEnd(start, end, timeRange.contains(end));
+      availableMeetings.add(availableMeeting);
+
+      start = timeRange.contains(end) ? end - 1 : end;
+      end = start;
+    }
+
+    return availableMeetings;
+  }
+
+  private boolean hasAttendeesAttending(Collection<String> eventAttendees, Collection<String> requestAttendees) {
+    for (String attendee: requestAttendees) {
+      if (eventAttendees.contains(attendee)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private List<TimeRange> mergeTimeRange(List<TimeRange> timeRanges) {
+    List<TimeRange> mergedTimeRanges = new ArrayList<>();
+
+    if (timeRanges == null) {
+      return mergedTimeRanges;
+    }
+
+    Collections.sort(timeRanges, TimeRange.ORDER_BY_START);
+
+    TimeRange last = null;
+    for (TimeRange timeRange: timeRanges) {
+      // The first TimeRange or no overlapping
+      if (last == null || !timeRange.overlaps(last)) {
+        mergedTimeRanges.add(timeRange);
+        last = timeRange;
+      } else {
+        TimeRange mergedTimeRange;
+        if (last.contains(timeRange)) {
+          continue;
+        }
+
+        if (last.end() == timeRange.end()) {
+          mergedTimeRange = TimeRange.fromStartEnd(last.start(), last.end(),
+                  last.contains(last.end()) || timeRange.contains(timeRange.end()));
+
+        } else {
+          mergedTimeRange = TimeRange.fromStartEnd(last.start(), timeRange.end(), timeRange.contains(timeRange.end()));
+        }
+
+        mergedTimeRanges.remove(mergedTimeRanges.size() - 1);
+        mergedTimeRanges.add(mergedTimeRange);
+        last = mergedTimeRange;
+      }
+    }
+
+    return mergedTimeRanges;
   }
 }
